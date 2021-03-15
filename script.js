3469a844d7f2f915ee6066cd9ecc0d0744599555
ru14 <74753345+ru14@users.noreply.github.com>
2021-03-15 00:01:31 -0700

    js modified

--- script.js
+++ script.js
@@ -19,28 +19,6 @@ function initMap() {
     });
 }
 
-// const api = {
-//     key: "f9ca789169d967eac2bc191ca630ca88",
-//     baseurl: "https://openweathermap.org/data/2.5",
-// }
-// const searchBtn = document.querySelector(".btn");
-// searchBtn.addEventListener("click", setQuery);
-
-// function setQuery(event) {
-//     const searchBox = document.getElementById("searchBox");
-//     const newCity = getResult(searchBox.value);
-//     console.log(newCity)
-
-// }
-// function getResult(query) {
-//     fetch(`${api.baseurl} weather?q=${query}&units=metric&APPID=${api.key}`)
-//     .then(weather => {
-//         return weather.json();
-//     }).then(displayResults);
-// }
-// function displayResults(weather) {
-//     console.log(weather);
-// }
 
 
 
