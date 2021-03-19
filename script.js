b4a8b275ec5fec46cd4a372bfe83425d2d7bb220
ru14 <74753345+ru14@users.noreply.github.com>
2021-03-18 22:28:34 -0700

    ad png and put for loop

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+let recentSearches = JSON.parse(localStorage.getItem("searches")) || [];
 
-function initMap() {
-    const searchElement = document.getElementById("searchBox");
+var initMap = function() {
+    let searchElement = document.getElementById("searchBox");
     let searchBox = new google.maps.places.SearchBox(searchElement);
     searchBox.addListener("places_changed", () => {
         const place = searchBox.getPlaces()[0];
@@ -18,63 +19,161 @@ function initMap() {
             })
             .then(data => {
                 console.log(data);
-                setWeatherData(data, place.formatted_address)
+                setWeatherData(data, place.formatted_address,);
+                recentSearches.push(place.formatted_address);
+                localStorage.setItem("searches",JSON.stringify(recentSearches))
+                console.log(recentSearches);
                 console.log(data);
             });
-        // let icon = `http://openweathermap.org/img/wn/` + data.weather[0].icon + `.png`;
-        // console.log(icon);
     });
+}
+
+for(i=(recentSearches.length-1);i>=(recentSearches.length-9);i--){
+    document.querySelector("#recentSearches").innerHTML+=
+    `<div class="city">
+   ${recentSearches[i]}
+    </div>`;
+}
+let searchButtons = document.querySelectorAll(".city")
+
+for (button of searchButtons){
+    button.addEventListenerz
+}
+
+// const icon = new Skycons({ color: "black" });
+const todaysDate = document.querySelector("[data-date]");
+const location2 = document.querySelector("[data-location]");
+const tempEle = document.querySelector("[data-Temp]");
+const humidity = document.querySelector("[data-Humidity]");
+const wind = document.querySelector("[data-Wind]");
+const uVindex = document.querySelector("[data-UVindex]");
+const summary = document.querySelector("[data-summary]");
+const tempMon = document.querySelector("[data-MonTemp]");
+const humMon = document.querySelector("[data-MonHumidity]");
+
+const tempTue = document.querySelector("[data-TueTemp]");
+const humTue = document.querySelector("[data-TueHumidity]");
+const tempWed = document.querySelector("[data-WedTemp]");
+const humWed = document.querySelector("[data-WedHumidity]");
+const tempThus = document.querySelector("[data-ThusTemp]");
+const humThus = document.querySelector("[data-ThusHumidity]");
+const tempFri = document.querySelector("[data-FriTemp]");
+const humFri = document.querySelector("[data-FriHumidity]");
+
+// icon.add("Icon", "clear_day");
+// icon.play();
+
+function setWeatherData(data, place) {
+    todaysDate.textContent = moment.unix(data.current.dt).format('MMMM Do YYYY, h:mm:ss A');
+    location2.textContent = place
+    tempEle.textContent = data.current.temp
+    humidity.textContent = data.current.humidity
+    wind.textContent = data.current.wind_speed
+    uVindex.textContent = data.current.uvi
+    summary.textContent = data.current.weather[0].description
+
+    // console.log(data.current.weather[0].main);
+   
+    console.log(data.daily.temp);
+   
+    setDailyWeatherData(data, place)
+}
+
+function setDailyWeatherData(data, place) {
+    tempMon.textContent =  data.daily[0].temp.max
+    humMon .textContent = data.daily[0].humidity  
+    tempTue.textContent = data.daily[1].temp.max
+    humTue .textContent = data.daily[1].humidity
+    tempWed.textContent = data.daily[2].temp.max
+    humWed.textContent = data.daily[2].humidity
+    tempThus.textContent = data.daily[3].temp.max
+    humThus.textContent = data.daily[3].humidity
+    tempFri.textContent = data.daily[4].temp.max
+    humFri.textContent = data.daily[4].humidity
+}
 
 
 
-    // const icon = new Skycons({ color: "black" });
-    const todaysDate = document.querySelector("[data-date]");
-    const location = document.querySelector("[data-location]");
-    const tempEle = document.querySelector("[data-Temp]");
-    const humidity = document.querySelector("[data-Humidity]");
-    const wind = document.querySelector("[data-Wind]");
-    const uVindex = document.querySelector("[data-UVindex]");
-    const summary = document.querySelector("[data-summary]");
-
-    // icon.add("Icon", "clear_day");
-    // icon.play();
-
-    function setWeatherData(data, place) {
-        todaysDate.textContent = moment.unix(data.current.dt).format('MMMM Do YYYY, h:mm:ss A');
-        location.textContent = place
-        tempEle.textContent = data.current.temp
-        humidity.textContent = data.current.humidity
-        wind.textContent = data.current.wind_speed
-        uVindex.textContent = data.current.uvi
-        summary.textContent = data.current.weather[0].description
-        console.log(data.current.weather[0].main);
-
-
-
-        // let resentSearch_stringyfy = JSON.stringify(data,);
-        // let resentSearch = localStorage.setItem("resentSearch", resentSearch_stringyfy);
-        // var savedValue = JSON.parse(localStorage.getItem("resentSearch"));
 
 
 
 
-        var savedValue = localStorage.getItem("resentSearch");
-        if (savedValue !== null) {
-            localStorage.setItem("resentSearch1", savedValue)
-        }
-        let resentSearch = localStorage.setItem("resentSearch", place);
 
 
 
 
-        if (savedValue) {
-            let newCity = document.querySelector(".city").innerHTML = savedValue;
-           
-        }
 
-        // console.log(savedValue);
 
-        // switch (data.current.weather[0].main) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     var savedValue = localStorage.getItem("resentSearch");
+
+//     if (savedValue !== null) {
+//         // localStorage.setItem("resentSearch7", savedValue)
+//         // localStorage.getItem("resentSearch6", savedValue)
+//         // localStorage.setItem("resentSearch6", savedValue)
+//         // localStorage.getItem("resentSearch5", savedValue)
+//         // localStorage.setItem("resentSearch5", savedValue)
+//         // localStorage.getItem("resentSearch4", savedValue)
+//         // localStorage.setItem("resentSearch4", savedValue)
+//         // localStorage.getItem("resentSearch3", savedValue)
+//         // localStorage.setItem("resentSearch4", savedValue)
+//         // localStorage.getItem("resentSearch3", savedValue)
+//         // localStorage.setItem("resentSearch3", savedValue)
+//         // localStorage.getItem("resentSearch2", savedValue)
+//         // localStorage.setItem("resentSearch2", savedValue)
+//         // localStorage.getItem("resentSearch1", savedValue)
+//         localStorage.setItem("resentSearch1", savedValue)
+
+//     }
+//     let resentSearch = localStorage.setItem("resentSearch", place);
+
+//     if (savedValue) {
+//         document.querySelector(".city").innerHTML = savedValue;
+//     }
+
+//     renderLastRegistered();
+
+// }
+// // console.log(savedValue);
+
+
+// function renderLastRegistered() {
+//     document.getElementById("cityOne").onclick = function moveContent() {
+//         document.getElementById("cityOne").innerHTML = searchElement.Value
+//     }
+// }
+
+
+    // set cityValue in search Element
+
+
+
+
+
+
+
+
+ // switch (data.current.weather[0].main) {
         //     case "Clouds":
         //         icon.add("Icon", "cloudy");
         //         break;
@@ -93,13 +192,8 @@ function initMap() {
         // }
         // // icon.set("icon", data.current.weather[0].icon);
         // icon.play();
-    }
 
-}
 
-function city(){
-    newCity.innerHTML = searchElement
-}
 //        // populate saved values
 //        var savedValue = localStorage.getItem(data,place);
 //     //    if (savedValue !== null){
@@ -118,6 +212,9 @@ function city(){
 //    }
 
 
+// let resentSearch_stringyfy = JSON.stringify(data,);
+        // let resentSearch = localStorage.setItem("resentSearch", resentSearch_stringyfy);
+        // var savedValue = JSON.parse(localStorage.getItem("resentSearch"));
 
 
 
@@ -125,13 +222,7 @@ function city(){
 
 
 
-
-
-
-
-
-
-// Use the [OpenWeather API](https://openweathermap.org/api) t
+        // Use the [OpenWeather API](https://openweathermap.org/api) t
 // o retrieve weather data for cities. The documentation includes a
 //  section called "How to start" that provides basic setup and usage i
 //  nstructions. 
